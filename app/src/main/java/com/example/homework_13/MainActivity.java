5d26c1258f2557fc0fcb94531d96f2edc67a3f76
Bekulan <totonya1509@bk.ru>
2019-12-11 16:57:01 +0600

    Integer.parseint

--- app/src/main/java/com/example/homework_13/MainActivity.java
+++ app/src/main/java/com/example/homework_13/MainActivity.java
@@ -3,65 +3,58 @@ package com.example.homework_13;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-TextView textView;
-
-TextView sum = textView;
+float volue1;
+float volue2;
+boolean plus2,minus2,multiply2,devite2;
+EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.result);
+
+        editText = findViewById(R.id.result);
+
+
 
 
     }
     public void onClick(View v){
-Integer.parseInt(textView+"");
-Integer.parseInt(sum+"");
 
         switch (v.getId()){
             case R.id.zero:
-                Integer.parseInt(0+"");
-                textView.append(0+"");
+                editText.setText(editText.getText()+"0");
                 break;
             case R.id.one:
-                textView.append(1+"");
+                editText.setText(editText.getText()+"1");
                 break;
             case R.id.two:
-                Integer.parseInt(2+"");
-                textView.append("2");
+               editText.setText(editText.getText()+"2");
                 break;
             case R.id.three:
-                Integer.parseInt(3+"");
-                textView.append("3");
+               editText.setText(editText.getText()+"3");
                 break;
             case R.id.four:
-                Integer.parseInt(4+"");
-                textView.append("4");
+                editText.setText(editText.getText()+"4");
                 break;
             case R.id.five:
-                Integer.parseInt(5+"");
-                textView.append("5");
+               editText.setText(editText.getText()+"5");
                 break;
             case R.id.six:
-                Integer.parseInt(6+"");
-                textView.append("6");
+                editText.setText(editText.getText()+"6");
                 break;
             case R.id.seven:
-                Integer.parseInt(7+"");
-                textView.append("7");
+                editText.setText(editText.getText()+"7");
                 break;
             case R.id.eight:
-                Integer.parseInt(8+"");
-                textView.append("8");
+               editText.setText(editText.getText()+"8");
                 break;
             case R.id.nine:
-                Integer.parseInt(9+"");
-                textView.append("9");
+               editText.setText(editText.getText()+"9");
                 break;
         }
 
@@ -71,13 +64,62 @@ Integer.parseInt(sum+"");
     public void onClick2 (View v){
         switch (v.getId()){
             case R.id.minus:
-               sum = sum-textView;
+                if (editText == null){
+                    editText.setText("");
+                }else {
+                    volue1 = Float.parseFloat(editText.getText()+"");
+                    minus2=true;
+                    editText.setText(null);
+                }
                 break;
             case R.id.c:
-                textView.setText("");
+                editText.setText("");
                 break;
             case R.id.plus:
+                if (editText==null){
+                    editText.setText("");
+                }else
+                    volue1=Float.parseFloat(editText.getText()+"");
+                     plus2=true;
+                     editText.setText(null);
                 break;
+            case R.id.devite:
+                if (editText==null){
+                    editText.setText("");
+                }else{
+
+                    volue1 = Float.parseFloat(editText.getText()+"");
+                    devite2=true;
+                    editText.setText(null);
+                }
+            case R.id.com:
+                String com2= editText.getText().toString();
+                if (TextUtils.isEmpty(com2)){
+                    editText.setText("");
+                }else{
+                    editText.setText(editText.getText()+",");
+                }
+                break;
+
+        }
+
+    }
+
+    public void onClick3(View v){
+        volue2 = Float.parseFloat(editText.getText()+"");
+        if (plus2==true){
+            editText.setText(volue1+volue2+"");
+            plus2=false;
+        }
+        if (minus2==true){
+            editText.setText(volue1-volue2+"");
+            plus2=false;
+        }if (devite2==true){
+            editText.setText(volue1/volue2+"");
+            devite2=false;
+        }if (multiply2==true){
+            editText.setText(volue1*volue2+"");
+            multiply2=false;
         }
 
     }
