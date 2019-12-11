80337fc83c3410aa14a1417922eb689a6640e31c
Bekulan <totonya1509@bk.ru>
2019-12-11 13:02:44 +0600

    Integer.parseint

--- app/src/main/java/com/example/homework_13/MainActivity.java
+++ app/src/main/java/com/example/homework_13/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 TextView textView;
 
-String sum;
+TextView sum = textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,38 +20,47 @@ String sum;
 
     }
     public void onClick(View v){
-
+Integer.parseInt(textView+"");
+Integer.parseInt(sum+"");
 
         switch (v.getId()){
             case R.id.zero:
                 Integer.parseInt(0+"");
-                textView.append("0");
+                textView.append(0+"");
                 break;
             case R.id.one:
-                textView.append("1");
+                textView.append(1+"");
                 break;
             case R.id.two:
+                Integer.parseInt(2+"");
                 textView.append("2");
                 break;
             case R.id.three:
+                Integer.parseInt(3+"");
                 textView.append("3");
                 break;
             case R.id.four:
+                Integer.parseInt(4+"");
                 textView.append("4");
                 break;
             case R.id.five:
+                Integer.parseInt(5+"");
                 textView.append("5");
                 break;
             case R.id.six:
+                Integer.parseInt(6+"");
                 textView.append("6");
                 break;
             case R.id.seven:
+                Integer.parseInt(7+"");
                 textView.append("7");
                 break;
             case R.id.eight:
+                Integer.parseInt(8+"");
                 textView.append("8");
                 break;
             case R.id.nine:
+                Integer.parseInt(9+"");
                 textView.append("9");
                 break;
         }
@@ -62,12 +71,13 @@ String sum;
     public void onClick2 (View v){
         switch (v.getId()){
             case R.id.minus:
-              // sum = textView-textView;
+               sum = sum-textView;
                 break;
             case R.id.c:
                 textView.setText("");
                 break;
-
+            case R.id.plus:
+                break;
         }
 
     }
